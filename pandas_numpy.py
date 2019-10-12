76918fa6017815c6bafc772c4f1908822d84e465
Talia <talia@Patricks-MBP15.local>
2019-10-11 22:57:46 -0700

    adding nike

---
+++ pandas_numpy.py
@@ -0,0 +1,80 @@
+from run_data_api import RunDataApiRequestor
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+# environment settings: 
+# pd.set_option('display.max_column',None)
+# pd.set_option('display.max_rows',None)
+# pd.set_option('display.max_seq_items',None)
+# pd.set_option('display.max_colwidth', 500)
+# pd.set_option('expand_frame_repr', True)
+
+def compare(row):
+    prev_distance = row.distance_in_km.shift(1)
+    this_distance = row["distance_in_km"]
+    this_date = row['y_m_d']
+    prev_date = row['y_m_d'].shift()
+    if prev_date == this_date and abs(this_distance - prev_distance) <= 1:
+        row["duplicate"] == True
+    return row
+
+def do_data_science():
+    requestor = RunDataApiRequestor()
+    activities = requestor.get_and_save_strava_activites()
+
+    df = pd.read_json(activities)
+
+    df = df[df.type == "Run"]
+
+    df['start_date'] = pd.to_datetime(df['start_date'], infer_datetime_format=True)
+    df['date_no_timestamp'] = df['start_date'].apply(lambda x: x.date())
+    df['y_m_d'] = df['start_date'].apply(lambda x: '{}-{}-{}'.format(x.year, x.month, x.day))
+
+    df['distance_in_km'] = df['distance'].apply(lambda x: x / 1000)
+
+    print('Total strava running distance:')
+    print(df["distance_in_km"].sum())
+
+    tag_duplicates_in_numpy(df)
+
+    # data_to_plot = df.groupby("date_no_timestamp")["distance_in_km"].sum()
+    # grouped = df.groupby(["y_m_d"])["distance_in_km"].agg('sum').plot(kind="bar")
+
+    df = df.sort_values(by=['y_m_d', 'distance_in_km'])
+
+    # df.groupby(["start_date"])["distance_in_km"].agg('sum').plot(kind="bar")
+    # fig, ax = plt.subplots(figsize=(10, 6))
+    # ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d %Y'))
+    # ax.bar(data_to_plot.index, data_to_plot[0], width=25, align='center')
+    # plt.show()
+
+def tag_duplicates_in_numpy(df):
+    numps = df.head(1).to_numpy(copy=False)
+    for i, val in enumerate(numps):
+        print(numps[i])
+        # if i != 0:
+            # prev_distance = numps[i-1]["distance_in_km"]
+            # this_distance = numps[i]["distance_in_km"]
+            # this_date = numps[i-1]['y_m_d']
+            # prev_date = numps[i]['y_m_d']
+            # if prev_date == this_date and abs(this_distance - prev_distance) <= 1:
+            #     print("Found a duplicate!")
+            #     numps[i]["duplicate"] = True
+            #     numps[i-1]["duplicate"] = True
+
+
+# compare stuff
+    # print(df.head(4))
+    # other_df = df.apply(compare, axis=1)
+
+    # for i, row in df.iterrows():
+    #     if i != 0:
+    #         # prev_distance = df.iloc[i-1]["distance_in_km"]
+    #         this_distance = df.iloc[:, i]["distance_in_km"]
+    #         # prev_date = df.iloc[i-1]['y_m_d']
+    #         this_date = df.iloc[:,i]['y_m_d']
+            # if prev_date == this_date and abs(this_distance - prev_distance) <= 1:
+            #     df.iloc[i]["duplicate"] = True
+            #     df.iloc[i-1]["duplicate"] = True
--- run_data_api.py
+++ run_data_api.py
@@ -1,19 +1,30 @@
 import requests
 import json
+from enum import Enum
+from pathlib import Path
+
+class ActivityType(Enum):
+    STRAVA = "strava"
+    NIKE = "nike"
+    APPLE = "apple"
 
 class RunDataApiRequestor():
 
     def __init__(self):
-        self.refresh_token = "1c6e10fadf51ddfbe7ab0512d901f408a96d4ceb"
-        self.access_token =  "71437b42db374d767663d71f8e9a7a676a5748ca"
-        self.athlete_id = "6473758"
 
+        self.strava_code="640bc0d5307f42a8ca73ec115b0f6ea51bad1a7b"
+
+        self.strava_refresh_token = "1c6e10fadf51ddfbe7ab0512d901f408a96d4ceb"
+        self.strava_access_token =  "6fe6e50a8194efdd78e0229269b466f6a8786350"
+       
+        self.strava_athlete_id = "6473758"
         self.strava_client_id = "17083"
-        self.strava_code="f0e32cc2d75e854965dee4e4c12981a91a725437"
         self.strava_client_secret = "9c4e8fbc7844f583b26888c6898e7d546e5adc3c"
 
         self.strava_callback_domain = "http://localhost"
         self.strava_scope= "activity:read_all,activity:write"
+
+        self.nike_access_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzMjdkOGU4LWNlNmMtNGI0MC1iYTdmLTRmYmI0OTM4Zjc4NnNpZyJ9.eyJ0cnVzdCI6MTAwLCJpYXQiOjE1NzA4NTkzOTAsImV4cCI6MTU3MDg2Mjk5MCwiaXNzIjoib2F1dGgyYWNjIiwianRpIjoiNmIwYTJmOTUtZGI2Ni00ZTk1LTkxZWEtZjE4MWJlNGQxNzg3IiwibGF0IjoxNTcwODU1NzIyLCJhdWQiOiJjb20ubmlrZS5kaWdpdGFsIiwic3ViIjoiY29tLm5pa2UuY29tbWVyY2UubmlrZWRvdGNvbS53ZWIiLCJzYnQiOiJuaWtlOmFwcCIsInNjcCI6WyJuaWtlLmRpZ2l0YWwiXSwicHJuIjoiOTU3MTQ5ODU4NiIsInBydCI6Im5pa2U6cGx1cyJ9.WnmBKUKj4Rt8STu1wMJmhfaHGTo-iQy6Sd4T3H0BaURr_hCzIO2WADabTIhUqYbdCHEoVCP0rmlrIdWCvUz6Whxj3uQ-sUB3AAZZUdhHxsSBUA0WoTuh8GUiWAc8OayAP-R4ORJN5chv2v1Q3mMNZh2Y5WG2yhQmLfgRt4S8azoNcPyAkaoePBkF-AXHPkyd_zTmUVsqgB58sRoxlDAVtSLX6SBNmx5IgsEyDZXYNm7I0qhncDfcyDMU7Azij0MIwYoZZQmoi0OvfOXTt0syGPSr17fo6--VPqqSJwHINXejH2cFIxdTkiuTKaY70s95GJBYp6Is_TLedK-5FBk_Ng"
 
     def get(self, url, headers = None, allow_redirects = False):
         print("making a get to {} with {}".format(url, headers))
@@ -41,6 +52,7 @@ class RunDataApiRequestor():
                         self.strava_callback_domain, 
                         self.strava_scope)
         print("Please authorize at the following url: {}".format(strava_auth_url))
+        print("You will need a code from this its redirect.")
 
     def do_strava_oauth(self):
         strava_oAuth_url = ("https://www.strava.com/oauth/token?"
@@ -52,38 +64,75 @@ class RunDataApiRequestor():
                         self.strava_client_secret, 
                         self.strava_code)
         json = self.post(strava_oAuth_url)
-        self.save_tokens(json)
+        self.strava_save_tokens(json)
 
-    def refresh_access_token(self):
+    def strava_refresh_access_token(self):
         strava_oAuth_url = ("https://www.strava.com/oauth/token?"
                     "refresh_token={}&"
-                    "grant_type=refresh_token").format(self.refresh_token)
+                    "grant_type=refresh_token").format(self.strava_refresh_token)
         json = self.post(strava_oAuth_url)
-        self.save_tokens(json)
+        self.strava_save_tokens(json)
 
-    def save_tokens(self, response):
-        self.refresh_token = response["refresh_token"]
-        self.access_token = response["access_token"]
-        self.athlete_id = response["athlete"]["id"]
-        print(self.refresh_token)
-        print(self.access_token)
+    def strava_save_tokens(self, response):
+        self.strava_refresh_token = response["refresh_token"]
+        self.strava_access_token = response["access_token"]
+        self.strava_athlete_id = response["athlete"]["id"]
+        print("Save these tokens...")
+        print("Access token:" + self.strava_access_token)
+        print("Refresh token:" + self.strava_refresh_token)
 
-    def get_and_save_activites(self):
-        filename = 'activities.txt'
+    def get_and_save_activities(self, activity_type):        
+        activities = self.get_json_from_file(activity_type.name)
+        if activities == None:
+            print(f"Fetching new activities for {activity_type}")
+            if activity_type == ActivityType.STRAVA:
+                headers = {'Authorization': 'Bearer {}'.format(self.strava_access_token)}
+                url = ("https://www.strava.com/api/v3/athlete/activities")
+                activities = self.get(url, headers=headers)
+            elif activity_type == ActivityType.NIKE:
+                headers = {'Authorization': 'Bearer {}'.format(self.nike_access_token)}
+                url = ("https://api.nike.com/sport/v3/me/activities/after_time/0")
+                activities = self.get(url, headers=headers)            
+            self.save_json_to_file(activity_type.name, activities)
+        else:
+            print(f"Using cached activities for {activity_type}")
+        return json.dumps(activities)
+    
+    def niki_paginate(self, activities):
+        if activities['paging']:
+            print('paging')
+            # url="https://api.nike.com/sport/v3/me/activities/after_id/${after_id}"
+
+    def save_json_to_file(self, filename, json_to_save):
+        filename = f'{filename}_activity.txt'
+        with open(filename, 'w+') as f:
+            json.dump(json_to_save, f, ensure_ascii=False, indent=4)
+
+    def get_json_from_file(self, filename):
+        filename = f'{filename}_activity.txt'
+        Path(filename).touch(exist_ok=True)
         with open(filename, 'r+') as f:
             try:
                 return json.dumps(json.load(f))
             except json.decoder.JSONDecodeError:
-                headers = {'Authorization': 'Bearer {}'.format(self.access_token)}
-                strava_get_activities_url = ("https://www.strava.com/api/v3/athlete/activities")
-                activities = self.get(strava_get_activities_url, headers=headers)
-                json.dump(activities, f, ensure_ascii=False, indent=4)
-                return json.dumps(activities)
+                print("Nothing in the file")
+                return None
+
+    # def get_and_save_activities(self, activity_type, headers, url):
+    #     filename = f'{activity_type.name}_activities.txt'
+    #     with open(filename, 'w+') as f:
+    #         try:
+    #             return json.dumps(json.load(f))
+    #         except json.decoder.JSONDecodeError:
+    #             activities = self.get(url, headers=headers)
+    #             json.dump(activities, f, ensure_ascii=False, indent=4)
+    #             return json.dumps(activities)
+
 
 # http GET "https://www.strava.com/api/v3/activities/{id}?include_all_efforts=" "Authorization: Bearer [[token]]"
-# requestor = RunDataApiRequestor()
-# # requestor.do_strava_auth()
-# # requestor.do_strava_oauth()
-# requestor.get_and_save_activites()
-
-
+requestor = RunDataApiRequestor()
+# requestor.do_strava_auth()
+# requestor.do_strava_oauth()
+# requestor.get_and_save_activities(ActivityType.STRAVA)
+activities = requestor.get_and_save_activities(ActivityType.NIKE)
+requestor.niki_paginate(activities)
