b6703d1b2d84b42f96f815e16eaee3a36d94b6a9
Talia <talia@Patricks-MBP15.local>
2019-10-13 21:28:48 -0700

    more pandas stuff : :

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import pytz
 from functools import reduce
 import pandas as pd
 import numpy as np
+import json
 
 requestor = JsonRunDataRequestor()
 
 def merge_strava_nike_apple_data():
     strava_data = requestor.get_json_activities(ActivityType.STRAVA)
 
-    merged_activity = []
+    merged_activity = pd.DataFrame({'start_time': pd.Timestamp, 'end_time': pd.Timestanp, 'duration': float, 'distance': float})
 
     for activity in strava_data:
         if activity['type'] == "Run":
@@ -43,6 +44,48 @@ def merge_strava_nike_apple_data():
                 ActivityType.APPLE, None)), axis=1)
     return merged_activity
 
+def merge_strava_nike_apple_data_more_pandas():  
+
+    all_activities = pd.DataFrame(columns=['start_time', 
+                                            'duration', 
+                                            'distance_in_km', 
+                                            'activity_type'])
+    
+    strava_data = json.dumps(requestor.get_json_activities(ActivityType.STRAVA))
+    df = pd.read_json(strava_data)
+    df = df[['distance', 
+        'elapsed_time', 
+        'start_date_local', 
+        'location_city', 
+        'average_speed', 
+        'max_speed', 
+        'type']]
+
+    df['start_time'] = df['start_date_local'].apply(lambda x: parse(x, tzinfos={"America/Vancouver"}))
+    df['distance_in_km'] = df['distance'].apply(lambda x: x / 1000)
+    df['activity_type'] = df['type']
+    df['duration'] = df['elapsed_time']
+    df = df.drop(columns=['start_date_local', 'distance', 'type', 'elapsed_time'], axis=1)
+
+    all_activities.append(df, sort=True)
+
+    nike_data = json.dumps(requestor.get_json_activities(ActivityType.NIKE))
+    df2 = pd.read_json(nike_data, typ='series', dtype={"articleId": np.float64})
+    df2 = df2[['type', 
+            'start_epoch_ms', 
+            'active_duration_ms']]
+
+    df2['start_time'] = df2['start_epoch_ms'].apply(lambda x: datetime.fromtimestamp(x / 1000, pytz.timezone('America/Vancouver')))
+    df2['distance_in_km'] = df2['distance'].apply(lambda x: x / 1000)
+    df2['activity_type'] = df2['type']
+    # df['duration'] = df['elapsed_time']
+
+    df
+
+
+    # df['date_no_timestamp'] = df['start_date'].apply(lambda x: x.date())
+    # df['y_m_d'] = df['start_date'].apply(lambda x: '{}-{}-{}'.format(x.year, x.month, x.day))
+
 def load_apple_workouts():
     print("Getting csv data for apple data")
     workouts_filepath = 'data/apple_health_export_csv/Workout.csv'
@@ -64,9 +107,10 @@ def calc_total_distance_run(activities):
     print(f"Total distance run: {sum}")
 
 def main():
-    activities = merge_strava_nike_apple_data()
-    remove_duplicates(activities)
-    calc_total_distance_run(activities)
+    # activities = merge_strava_nike_apple_data()
+    # remove_duplicates(activities)
+    # calc_total_distance_run(activities)
+    merge_strava_nike_apple_data_more_pandas()
 
 if __name__ == "__main__":
     main()
